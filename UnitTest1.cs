927b37730f38d0796f6e4350de146eca5dba737e
Sergi-0 <144801688+Sergi-0@users.noreply.github.com>
2024-09-19 22:04:46 +0300

    Add files via upload

---
+++ UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab1_2;
+
+namespace test1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [DataTestMethod]
+        [DataRow(1, 2, 2)]
+        [DataRow(2, 2, 4)]
+        [DataRow(1, 4, 4)]
+        public void TestMethod1(int a, int b, int c)
+        {
+            Rectangle rec = new Rectangle(a,b);
+            double area1 = rec.CalculateArea();
+            Assert.AreEqual(c, area1);
+        }
+
+        [DataTestMethod]
+        [DataRow(3, 2, 10)]
+        [DataRow(2, 5, 14)]
+        [DataRow(5, 1, 12)]
+        public void TestMethod2(int a, int b, int c)
+        {
+            Rectangle rec = new Rectangle(a, b);
+            double area1 = rec.CalculatePerimeter();
+            Assert.AreEqual(c, area1);
+        }
+
+    }
+}
